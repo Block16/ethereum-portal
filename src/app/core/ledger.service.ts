13f47b137ec63915b6e79d74f143d5bfaa27cfa2
Andrew Donley <andrewjdonley@gmail.com>
2018-03-23 22:14:28 -0700

    working on ledger

--- src/app/core/ledger.service.ts
+++ src/app/core/ledger.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import Transport from '@ledgerhq/hw-transport-u2f';
+// import Transport from "@ledgerhq/hw-transport-node-hid";
 import Eth from '@ledgerhq/hw-app-eth';
+import { Observable } from 'rxjs/Observable';
+import { isNullOrUndefined } from 'util';
 
 @Injectable()
 export class LedgerService {
 
+  // Don't use these instance variables in the functions
+  private transport: Transport;
+  private eth: Eth;
+
   constructor() { }
+
+  private getEthInstance(): Observable<Eth> {
+    return Observable.create(observer => {
+      this.getTransport().subscribe(transport => {
+        if (isNullOrUndefined(this.eth)) {
+          this.eth = new Eth(transport);
+        }
+        observer.next(this.eth);
+        observer.complete();
+      });
+    });
+  }
+
+  /**
+   * Get the browser UF2 transport
+   * @returns {Observable<>}
+   */
+  private getTransport(): Observable<Transport> {
+    return Observable.create(observer => {
+      if (isNullOrUndefined(this.transport)) {
+        Transport.create().then(transport => {
+          this.transport = transport;
+          observer.next(this.transport);
+          observer.complete();
+        });
+      } else {
+        observer.next(this.transport);
+        observer.complete();
+      }
+    });
+  }
+
+  public getEthereumAddress(): Observable<string> {
+    return Observable.create(observer => {
+      this.getEthInstance().subscribe(eth => {
+        this.eth.getAddress("44'/60'/0'/0'/0").then(o => {
+          observer.next(o.address);
+          observer.complete();
+        }, err => {
+          console.log("Couldn't get ethereum address from ledger");
+          console.log(err);
+          observer.error(err.toString());
+          observer.complete();
+        });
+      });
+    });
+  }
 
 }
--- src/app/public/index/index.component.ts
+++ src/app/public/index/index.component.ts
@@ -1,12 +1,12 @@
 import {Component, ElementRef, EventEmitter, HostListener, OnInit, Output, ViewChild} from '@angular/core';
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+
 import {DataShareService} from "../../core/data-share.service";
 import {LedgerService} from '../../core/ledger.service';
 import {TrezorConnectService} from '../../core/trezor-connect.service';
 import {Web3Service} from "../../core/web3.service";
 
 enum AuthState {
-  trezor, bitbox, metamask, utcFile, privateKey, ledger
+  none, trezor, bitbox, metamask, utcFile, privateKey, ledger
 }
 
 @Component({
@@ -18,30 +18,24 @@ export class IndexComponent implements OnInit {
   @ViewChild('_recentTransactions') _recentTransactions: ElementRef;
   public AuthState = AuthState;
   public currentAuth: AuthState;
-  
+
   public recentTransactions = [];
   public newTransaction = {};
-  
+
   // document states
   private windowHeight: number;
   private windowWidth: number;
   private windowMin: number;
   private windowMax: number;
-  
+
   // UI states
-  public showQR: boolean = false;
-  
+  public showQR = false;
+
   // Styles
   public newTransactionStyle = {};
   public newTransactionCircleStyle = {};
-  
+
   @Output() ethereumAddressChange: EventEmitter<string> = new EventEmitter<string>();
-  /*
-  @Output() mobileChange: EventEmitter<string> = new EventEmitter<string>();
-  @Input() set mobile(value) {
-    this.msisdn_confirm = this.msisdn = value;
-  }
-  */
   public ethereumAddress: string;
 
   constructor(
@@ -64,25 +58,41 @@ export class IndexComponent implements OnInit {
       this.updateAddress(address);
     });
   }
-  
+
+  ledgerAuthState() {
+    this.currentAuth = AuthState.ledger;
+    this.ledgerService.getEthereumAddress().subscribe((address: string) => {
+      this.ethereumAddress = address;
+      this.updateAddress(address);
+    });
+  }
+
   generateTransaction() {
     this.newTransaction = this.randomTransaction();
   }
-  
+
   randomTransaction() {
-    var addresses = ['0x2a65Aca4D5fC5B5C859090a6c34d164135398226','0x9034C5691E4CF92507E79a5A29D8e162b9506cD9','0x4Cd988AfBad37289BAAf53C13e98E2BD46aAEa8c','0xf73C3c65bde10BF26c2E1763104e609A41702EFE','0x8d12A197cB00D4747a1fe03395095ce2A5CC6819','0x4781BEe730C9056414D86cE9411a8fb7FF02219f','0x2ddb2555c3C7Ad23991125CAa4775E19b93204b9'];
-    var toAddress = addresses[Math.floor(Math.random()*addresses.length)];
-    var fromAddress = addresses[Math.floor(Math.random()*addresses.length)];
-    var statuses = ['processing', 'confirmed', 'failed'];
+    const addresses = [
+      '0x2a65Aca4D5fC5B5C859090a6c34d164135398226',
+      '0x9034C5691E4CF92507E79a5A29D8e162b9506cD9',
+      '0x4Cd988AfBad37289BAAf53C13e98E2BD46aAEa8c',
+      '0xf73C3c65bde10BF26c2E1763104e609A41702EFE',
+      '0x8d12A197cB00D4747a1fe03395095ce2A5CC6819',
+      '0x4781BEe730C9056414D86cE9411a8fb7FF02219f',
+      '0x2ddb2555c3C7Ad23991125CAa4775E19b93204b9'
+    ];
+    const toAddress = addresses[Math.floor(Math.random() * addresses.length)];
+    const fromAddress = addresses[Math.floor(Math.random() * addresses.length)];
+    const statuses = ['processing', 'confirmed', 'failed'];
     // var status = statuses[Math.floor(Math.random()*statuses.length)]
-    var status = 'processing';
-    var confirmations = 0;
-    if (status == 'confirmed') {
+    let status = 'processing';
+    let confirmations = 0;
+    if (status === 'confirmed') {
       confirmations = Math.floor(Math.random() * 20);
     }
-    var tokens = ['ETH','SPHTX','WETH','UKG','THETA','ZRX','CS','MAN','REM'];
-    var token = tokens[Math.floor(Math.random()*tokens.length)];
-    var amount = Math.floor(Math.random() * 1000000);
+    const tokens = ['ETH', 'SPHTX', 'WETH', 'UKG', 'THETA', 'ZRX', 'CS', 'MAN', 'REM'];
+    const token = tokens[Math.floor(Math.random() * tokens.length)];
+    const amount = Math.floor(Math.random() * 1000000);
     return {
       'toAddress': toAddress,
       'fromAddress': fromAddress,
@@ -91,20 +101,20 @@ export class IndexComponent implements OnInit {
       'token': token,
       'amount': amount,
       'created': new Date()
-    }
+    };
   }
-  
+
   showApproveNewTransaction() {
     this.newTransaction = this.randomTransaction();
     // size and positionnew transaction element
     this.setNewTransactionViewFullscreen();
     // this.setNewTransactionStyle();
   }
-  
+
   transactionSigned() {
-    
+
     this.setNewTransactionViewToDock();
-        
+
     // setTimeout(() => {
     //   this.recentTransactions.push(this.newTransaction);
     //   this.dataShareService.recentTransactions.next(this.recentTransactions);
@@ -113,7 +123,7 @@ export class IndexComponent implements OnInit {
     //   this.setNewTransactionStyle();
     // }, 500)
   }
-  
+
   toggleAuthState(authState: AuthState) {
     this.currentAuth = authState;
   }
@@ -127,77 +137,77 @@ export class IndexComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.recentTransactions.push(this.randomTransaction())
+    this.recentTransactions.push(this.randomTransaction());
     this.dataShareService.recentTransactions.next(this.recentTransactions);
-    this.callibratePage();
+    this.calibratePage();
     this.resetNewTransactionView();
   }
-  
+
   fullTransactionViewCircleRadius() {
     return Math.sqrt(Math.pow(this.windowMax / 2, 2) + Math.pow(this.windowMin / 2, 2));
   }
-  
+
   resetNewTransactionView() {
-    var r = this.fullTransactionViewCircleRadius();
-    var leftOffset = (r - this.windowWidth / 2) * -1;
-    var topOffset = (r + this.windowHeight / 2 + this.windowHeight);
-    var transactionTransform = 'translate(0px,' + this.windowHeight + 'px) scale(.15)';
-    var circleTransform = 'translate(' + leftOffset + 'px,' + topOffset + 'px) scale(.15)';
-    
+    const r = this.fullTransactionViewCircleRadius();
+    const leftOffset = (r - this.windowWidth / 2) * -1;
+    const topOffset = (r + this.windowHeight / 2 + this.windowHeight);
+    const transactionTransform = 'translate(0px,' + this.windowHeight + 'px) scale(.15)';
+    const circleTransform = 'translate(' + leftOffset + 'px,' + topOffset + 'px) scale(.15)';
+
     this.newTransactionCircleStyle['transition'] = '0s';
     this.newTransactionCircleStyle['width'] = (r * 2) + 'px';
     this.newTransactionCircleStyle['height'] = (r * 2) + 'px';
-    
+
     this.newTransactionStyle['transform'] = transactionTransform;
     this.newTransactionCircleStyle['transform'] = circleTransform;
   }
-  
+
   setNewTransactionViewFullscreen() {
-    var r = this.fullTransactionViewCircleRadius();
-    var leftOffset = (r - this.windowWidth / 2) * -1;
-    var topOffset = (r - this.windowHeight / 2) * -1;
-    var transactionTransform = 'translate(0,0)';
-    var circleTransform = 'translate(' + leftOffset + 'px,' + topOffset + 'px) scale(1)';
-    
+    const r = this.fullTransactionViewCircleRadius();
+    const leftOffset = (r - this.windowWidth / 2) * -1;
+    const topOffset = (r - this.windowHeight / 2) * -1;
+    const transactionTransform = 'translate(0,0)';
+    const circleTransform = 'translate(' + leftOffset + 'px,' + topOffset + 'px) scale(1)';
+
     this.newTransactionCircleStyle['width'] = (r * 2) + 'px';
     this.newTransactionCircleStyle['height'] = (r * 2) + 'px';
     this.newTransactionCircleStyle['transition'] = '.5s';
-    
+
     this.newTransactionStyle['opacity'] = '1';
     this.newTransactionStyle['transform'] = transactionTransform;
     this.newTransactionCircleStyle['transform'] = circleTransform;
   }
-  
+
   setNewTransactionViewToDock() {
-    
-    var transactionDotSize = '12.8px';
-    var transactionDotXOffset = '24px';
-    var marketingHeight = this._recentTransactions.nativeElement.offsetHeight;
-    var transactionYOffset = this._recentTransactions.nativeElement.offsetHeight + this._recentTransactions.nativeElement.offsetTop;
-    var transactionTransform = 'translate(' +transactionDotXOffset + ',' + transactionYOffset + 'px)';
+
+    const transactionDotSize = '12.8px';
+    const transactionDotXOffset = '24px';
+    const marketingHeight = this._recentTransactions.nativeElement.offsetHeight;
+    const transactionYOffset = this._recentTransactions.nativeElement.offsetHeight + this._recentTransactions.nativeElement.offsetTop;
+    const transactionTransform = 'translate(' + transactionDotXOffset + ',' + transactionYOffset + 'px)';
     this.newTransactionCircleStyle['transform'] = transactionTransform;
     this.newTransactionCircleStyle['height'] = transactionDotSize;
     this.newTransactionCircleStyle['width'] = transactionDotSize;
-    
+
     this.newTransactionStyle['opacity'] = '0';
     setTimeout(() => {
       this.recentTransactions.push(this.newTransaction);
       this.dataShareService.recentTransactions.next(this.recentTransactions);
       this.resetNewTransactionView();
-    }, 500)
+    }, 500);
   }
-  
-  callibratePage() {
+
+  calibratePage() {
     this.windowHeight = window.innerHeight;
     this.windowWidth = window.innerWidth;
     this.windowMin = Math.min(this.windowWidth, this.windowHeight);
     this.windowMax = Math.max(this.windowWidth, this.windowHeight);
   }
-  
+
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     this.windowHeight = event.target.innerHeight;
     this.windowWidth = event.target.innerWidth;
-    this.callibratePage();
+    this.calibratePage();
   }
 }
