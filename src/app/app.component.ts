03f3022a88364de9ac6b126cbd303e7cb531c458
Kevin Thrasher <kevinthr@gmail.com>
2018-03-29 19:18:05 -0700

    initial state styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+<<<<<<< Updated upstream
 export class AppComponent { }
+=======
+export class AppComponent {
+	
+	public defaultPreferences = {
+		'manualGas': false,
+		'showGenerated': true,
+		'theme': 'Default',
+		'denomination': 'None'
+	}
+	
+	constructor(private dataShareService: DataShareService) {
+		
+	}
+	
+	public ngOnInit(): void {
+		this.dataShareService.userPreferences.next(this.defaultPreferences);
+		console.log(this.dataShareService.getUserPreferences());
+	}
+}
+>>>>>>> Stashed changes
