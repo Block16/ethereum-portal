ea82f410aac5c376cc300779c6b62dd447a1d710
Kevin Thrasher <kevinthr@gmail.com>
2018-04-10 17:28:06 -0700

    button component changes

--- src/app/shared/ui-elements/button/button.component.ts
+++ src/app/shared/ui-elements/button/button.component.ts
@@ -1,4 +1,4 @@
-import {Attribute, Component, HostBinding, Input, OnInit, OnDestroy} from '@angular/core';
+import {Attribute, Component, HostBinding, HostListener, Input, OnInit, OnDestroy} from '@angular/core';
 import {Subscription} from "rxjs/Subscription";
 import {ThemeService} from "../../../core/theme.service";
 import {Theme} from "../../model/theme/theme";
@@ -8,25 +8,42 @@ import {Theme} from "../../model/theme/theme";
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   host: {
-    tabindex: '0'
+    'tabindex': '0',
+    '[class.accent]': 'this.accent', 
+    '[class.hover]': 'this.hover',
+    '[style.background]': `this.accent ? 
+                           this.theme.accentColor :
+                           this.theme.primaryColor`,
+    '[style.color]': `this.theme.secondaryColor`
   }
 })
 
 export class ButtonComponent implements OnInit {
 
 	@HostBinding('class.disabled') disabled = false;
-  @HostBinding('class.accent') accent;
+  @HostListener('mouseover') onMouseOver() {
+     this.hover = true;
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+     this.hover = false;
+  }
 	@Input() kind: string;
   @Input('disabled') disabledInput = false;
 
   public theme: Theme;
+  private hover: boolean = false;
   private themeSubscription: Subscription;
 
 	constructor(
     @Attribute('size') private size: string = 'normal',
-    @Attribute('accent') private accentAttribute: boolean = false,
+    @Attribute('accent') private accent: boolean|null,
+    @Attribute('small') private small: boolean|null,
+    @Attribute('submit') private submit: boolean|null,
     private themeService: ThemeService
     ) {
+    this.accent = (accent != null);
+    this.small = (small != null);
+    this.submit = (submit != null);
 	  this.themeSubscription = this.themeService.theme.subscribe(theme => {
 	    this.theme = theme;
 	  });
