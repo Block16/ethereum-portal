14a4c3feb11c7110d404aacf74025b17da471b52
Andrew Donley <andrewjdonley@gmail.com>
2018-04-14 19:43:26 -0700

    json typing with tokendata

--- src/app/core/token-symbol.service.ts
+++ src/app/core/token-symbol.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { EthereumTokens } from "./ethereum-tokens";
+import * as tokendata from './static-models/ethereum-tokens.json';
 
 @Injectable()
 export class TokenSymbolService {
-	
+
   constructor() {
-  	console.log(EthereumTokens);
+  	console.log(tokendata);
   }
-  
+
   public checkTokenSymbol(address) {
   	// let result = EthereumTokens.filter(function( obj ) {
   	//   return obj.contract_address == address;
   	// });
   	return 'hi';
   }
-  
+
 }
