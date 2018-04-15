4f54b71197e5e582cc62c655d40250f0f7c43603
Andrew Donley <andrewjdonley@gmail.com>
2018-04-15 16:52:12 -0700

    find on casted type

--- src/app/core/token-symbol.service.ts
+++ src/app/core/token-symbol.service.ts
@@ -4,19 +4,12 @@ import * as tokendata from './static-models/ethereum-tokens.json';
 
 @Injectable()
 export class TokenSymbolService {
-
   constructor() {
     console.log(tokendata);
   }
 
   public checkTokenSymbol(address) {
-    // should use a find here instead, filter could be empty which would throw an array out of bounds error
-    // !!!!!
-    // const result = (<any[]>tokendata).filter((obj) => {
-    //   return obj.contract_address === address;
-    // });
-    const result = tokendata.find( obj => obj.contract_address === address );
-    return result[0];
+    return (<any[]>tokendata).find( obj => obj.contract_address === address );
   }
 
 }
