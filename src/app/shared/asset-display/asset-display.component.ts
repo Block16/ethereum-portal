9f160f1c6142b6029eb4e16233ac078c23013f85
Kevin Thrasher <kevinthr@gmail.com>
2018-04-25 17:56:52 -0700

    balance pipes work

--- src/app/shared/asset-display/asset-display.component.ts
+++ src/app/shared/asset-display/asset-display.component.ts
@@ -1,31 +1,45 @@
 import {Component, CUSTOM_ELEMENTS_SCHEMA, Input, OnInit} from '@angular/core';
 import {EthereumAsset} from "../model/ethereum-asset";
+import {Subscription} from "rxjs/Subscription";
+import {ThemeService} from "../../core/theme.service";
+import {Theme} from "../model/theme/theme";
 
 @Component({
   selector: 'app-asset-display',
   templateUrl: './asset-display.component.html',
-  styleUrls: ['./asset-display.component.scss']
+  styleUrls: ['./asset-display.component.scss'],
+  host: {
+    '[class]': '"no-wrap lining-proportional"',
+  }
 })
 export class AssetDisplayComponent {
-  public nonGreyValue: string;
-  public greyValue: string;
+  public nonGrayValue: string;
+  public nonGrayValueNumber: number;
+  public grayValue: string;
+  
+  public theme: Theme;
+  private themeSubscription: Subscription;
 
   @Input()
   set ethereumAsset(a: EthereumAsset) {
     const amount = a.calculatedAmount.toString(10);
     const index = amount.indexOf(".");
-    this.greyValue = "";
+    this.grayValue = "";
 
     if (index !== -1) {
-      this.nonGreyValue = amount.substring(0, index + 3);
-      this.greyValue = amount.substring(index + 4);
+      this.nonGrayValueNumber = Number(amount.substring(0, index + 3));
+      this.grayValue = amount.substring(index + 4);
     } else {
-      this.greyValue = amount;
+      this.grayValue = amount;
     }
   }
-
-  constructor() {
-    this.greyValue = "";
-    this.nonGreyValue = "";
+  constructor(private themeService: ThemeService) {
+    
+    this.themeSubscription = this.themeService.theme.subscribe(theme => {
+      this.theme = theme;
+    });
+    
+    this.grayValue = "";
+    this.nonGrayValue = "";
   }
 }
